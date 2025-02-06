674c166bdfbadb39ce8b605eaa1d7f440386a434
root <root@localhost.localdomain>
2025-02-06 06:00:19 +0000

    Fix: a little mistake

--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -1,5 +1,5 @@
--- if your mason download has connection error,use the following line.
--- lvim.builtin.mason.registries = { "github:mason-org/mason-registry@2025-02-05-tasty-liar" }
+-- use one version to avoid erroer
+lvim.builtin.mason.registries = { "github:mason-org/mason-registry@2025-02-05-tasty-liar" }
 
--- speed up the mason download of github
-lvim.builtin.mason.github = { download_url_template = "https://051030.xyz/%s/releases/download/%s/%s" }
+-- if your mason download has connection error,use the following line.
+-- lvim.builtin.mason.github = { download_url_template = "https://051030.xyz/%s/releases/download/%s/%s" }
